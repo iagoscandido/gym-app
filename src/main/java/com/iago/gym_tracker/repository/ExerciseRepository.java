f4ef558dc354dd18e3231c7d8790ef5c12df3915
iago silva <iago.scandido3@gmail.com>
2024-11-02 19:29:54 -0300

    feat: change Generics name to actual name convention

--- src/main/java/com/iago/gym_tracker/repository/ExerciseRepository.java
+++ src/main/java/com/iago/gym_tracker/repository/ExerciseRepository.java
@@ -1,10 +1,10 @@
 package com.iago.gym_tracker.repository;
 
-import com.iago.gym_tracker.model.ExerciseModel;
+import com.iago.gym_tracker.model.ExerciseEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ExerciseRepository extends JpaRepository<ExerciseModel, Long> {
+public interface ExerciseRepository extends JpaRepository<ExerciseEntity, Long> {
 
 }
