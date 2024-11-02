30cb001f178ffab6e739b5b2a643ef48feeb4241
iago silva <iago.scandido3@gmail.com>
2024-11-02 19:28:46 -0300

    feat: change muscle field insertion

--- src/main/java/com/iago/gym_tracker/service/ExerciseService.java
+++ src/main/java/com/iago/gym_tracker/service/ExerciseService.java
@@ -1,11 +1,12 @@
 package com.iago.gym_tracker.service;
 
-import com.iago.gym_tracker.EquipmentEnum;
-import com.iago.gym_tracker.MuscleEnum;
+import com.iago.gym_tracker.enums.EquipmentEnum;
 import com.iago.gym_tracker.dto.ExecutionDto;
 import com.iago.gym_tracker.dto.ExerciseDto;
-import com.iago.gym_tracker.model.ExecutionModel;
-import com.iago.gym_tracker.model.ExerciseModel;
+import com.iago.gym_tracker.dto.MuscleDto;
+import com.iago.gym_tracker.model.ExecutionEntity;
+import com.iago.gym_tracker.model.ExerciseEntity;
+import com.iago.gym_tracker.model.MuscleEntity;
 import com.iago.gym_tracker.repository.ExerciseRepository;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -20,30 +21,36 @@ import java.util.List;
 public class ExerciseService {
     private final ExerciseRepository repository;
 
-    public ExerciseModel create(@Valid ExerciseDto dto) {
+    public ExerciseEntity create(@Valid ExerciseDto dto) {
 
         log.info("{}", dto);
 
-        ExerciseModel entity = new ExerciseModel();
+        ExerciseEntity entity = new ExerciseEntity();
         entity.setName(dto.getName());
         entity.setEquipment(EquipmentEnum.valueOf(dto.getEquipment()));
-        entity.setMuscles(dto.getMuscles().stream().map(MuscleEnum::valueOf).toList());
         entity.setName(dto.getName());
-
-        List<ExecutionModel> executions = dto.getExecutions().stream().map(this::mapToExecutionModel).toList();
+        List<MuscleEntity> muscles = dto.getMuscles().stream().map(this::mapToMuscleModel).toList();
+        entity.setMuscles(muscles);
+        List<ExecutionEntity> executions = dto.getExecutions().stream().map(this::mapToExecutionModel).toList();
         entity.setExecutions(executions);
 
         return repository.save(entity);
     }
 
 
-    private ExecutionModel mapToExecutionModel(ExecutionDto dto) {
-        ExecutionModel model = new ExecutionModel();
+    private ExecutionEntity mapToExecutionModel(ExecutionDto dto) {
+        ExecutionEntity model = new ExecutionEntity();
         model.setWeight(dto.getWeight());
         model.setRepetitions(dto.getRepetitions());
         model.setRestTime(dto.getRestTime());
         model.setSets(dto.getSets());
 
+        return model;
+    }
+
+    private MuscleEntity mapToMuscleModel(MuscleDto dto) {
+        MuscleEntity model = new MuscleEntity();
+        model.setName(dto.getName());
         return model;
     }
 
