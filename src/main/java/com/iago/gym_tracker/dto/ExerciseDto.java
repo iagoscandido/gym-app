19cf8f5f9d886fbf40a1f1d49158f5fece03976c
iago silva <iago.scandido3@gmail.com>
2024-11-02 19:30:31 -0300

    feat: change muscle field

--- src/main/java/com/iago/gym_tracker/dto/ExerciseDto.java
+++ src/main/java/com/iago/gym_tracker/dto/ExerciseDto.java
@@ -7,13 +7,11 @@ import lombok.NoArgsConstructor;
 import java.util.List;
 
 @Data
-@NoArgsConstructor
-
 public class ExerciseDto {
     @NotBlank(message = "a name must be provided")
     private String name;
-    private List<String> muscles;
     private String equipment;
     private List<ExecutionDto> executions;
+    private List<MuscleDto> muscles;
 }
 
